b536c4984ede10b9cbea5e1909a10b3018e2eb2b
Josue-A-D-P <josueporto59@gmail.com>
2024-01-07 21:19:46 -0300

    Criando app

--- projeto1/urls.py
+++ projeto1/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from recipes.views import home, sobre, contato
 
-def my_view (request):
-    return HttpResponse("Uma linda string")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', home), #Home
+    path('sobre/', sobre), #/sobre/
+    path('contato/', contato), #/contato/
 ]
