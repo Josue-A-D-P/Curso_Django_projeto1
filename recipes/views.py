d5ed0755f27e5c0f601b0b4a4b2aec7bc86f8c7f
Josue-A-D-P <josueporto59@gmail.com>
2024-01-26 19:18:47 -0300

    criando urls para as views

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home (request):
-    return render(request, 'recipes/pages/home.html',)
+    return render(request, 'recipes/pages/home.html', context={
+        'name' : 'Josué Porto'
+    })
+
+def recipe (request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name' : 'Josué Porto'
+    })
